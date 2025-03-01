4a76bba4b05228c78685f54ed17e2788bfb45e84
ericlbmaster <ericlb.mx@gmail.com>
2025-02-28 22:46:19 -0600

    Adding DB_URL connection string.

--- src/config.js
+++ src/config.js
@@ -5,3 +5,4 @@ export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'mypassword'
 export const DB_NAME = process.env.DB_NAME || 'usersdb'
 export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_URL = process.env.DB_URL || "the.url"
